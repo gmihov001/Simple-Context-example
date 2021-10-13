3660b6084d1023337ec3eb18ae04ac4c05d865d7
George Mihov <gmihov001@gmail.com>
2021-10-13 19:41:36 +0000

    Updates

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,24 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import { Context } from "../store/appContext";
 
+//without Context
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+	const [color, setColor] = useState("red");
+	const [user, setUser] = useState("George");
+
+	const changeUser = (name, color) => {
+		setColor(color);
+		setUser(name);
+	};
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Color Demonstrator</h1>
 			<p>
-				<div className="colorBox" style={{ background: store.color }}>
+				<div className="colorBox" style={{ background: color }}>
 					<div className="userName">
-						<h2>{store.user}</h2>
+						<h2>{user}</h2>
 					</div>
 				</div>
 			</p>
-			<a href="#" className="btn btn-success" onClick={() => actions.changeUser()}>
+			<a href="#" className="btn btn-success" onClick={() => changeUser("Matthew", "orange")}>
 				Change user
 			</a>
 		</div>
 	);
 };
+
+//with Context
+// export const Home = () => {
+// 	const { store, actions } = useContext(Context);
+
+// 	return (
+// 		<div className="text-center mt-5">
+// 			<h1>Color Demonstrator</h1>
+// 			<p>
+// 				<div className="colorBox" style={{ background: store.color }}>
+// 					<div className="userName">
+// 						<h2>{store.user}</h2>
+// 					</div>
+// 				</div>
+// 			</p>
+// 			<a href="#" className="btn btn-success" onClick={() => actions.changeUser("Matthew", "orange")}>
+// 				Change user
+// 			</a>
+// 		</div>
+// 	);
+// };
